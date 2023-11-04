a175cd261ff4067fc12edc3ba63e7230a36d1e4a
Aaron Evans <aarone@outlook.com.au>
2023-11-04 16:24:34 +1100

    added required providers / removed provider config

--- provides.tf
+++ provides.tf
@@ -1,6 +1,8 @@
-provider "vsphere" {
-  user                 = var.vsphere_user
-  password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = true
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.5.1"
+    }
+  }
 }
