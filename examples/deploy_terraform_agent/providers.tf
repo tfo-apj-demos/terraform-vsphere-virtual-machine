3335b07c83d6501712f7ca42d8501c8931986b77
Aaron Evans <aarone@outlook.com.au>
2023-11-04 16:25:54 +1100

    added provider config for child module

--- examples/deploy_terraform_agent/providers.tf
+++ examples/deploy_terraform_agent/providers.tf
@@ -4,9 +4,20 @@ terraform {
       source  = "hashicorp/tfe"
       version = "0.49.2"
     }
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.5.1"
+    }
   }
 }
 
 provider "tfe" {
   organization = var.organization
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
 }
