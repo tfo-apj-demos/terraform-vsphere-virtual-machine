20ac90292f188f8efd1c0a0e22f5c3b98178721b
Grant Orchard <go@hashicorp.com>
2023-11-08 16:46:24 +1100

    hygeine

--- examples/deploy_terraform_agent/main.tf
+++ examples/deploy_terraform_agent/main.tf
@@ -1,9 +1,3 @@
-# data "hcp_packer_image" "base-ubuntu-2204" {
-#   bucket_name    = "base-ubuntu-2204"
-#   channel        = "latest"
-#   cloud_provider = "vsphere"
-#   region         = var.datacenter
-# }
 locals {
   user_list = yamldecode(file("${path.module}/templates/users.yaml"))["users"]
 }
@@ -19,17 +13,9 @@ resource "tfe_agent_token" "this" {
 }
 
 module "vm" {
-  providers = {
-    vsphere = vsphere
-  }
-  
   for_each = toset(local.user_list)
 
   source = "../.."
-
-  vsphere_user      = var.vsphere_user
-  vsphere_password  = var.vsphere_password
-  vsphere_server    = var.vsphere_server
   hostname          = format("%s-tfc-agent", each.value) # This appends -tfc-agent to each username
   datacenter        = var.datacenter
   cluster           = var.cluster
@@ -37,14 +23,9 @@ module "vm" {
   folder_path       = var.folder_path
   networks          = var.networks
   template          = var.vsphere_template_name
-  #template          = data.hcp_packer_image.base-ubuntu-2204.cloud_image_id
 
   userdata = templatefile("${path.module}/templates/userdata.yaml.tmpl", {
     agent_token = tfe_agent_token.this.token
     agent_name  = format("%s-tfc-agent", each.value) # Also append -tfc-agent for the agent_name
   })
-
-  # metadata = templatefile("${path.module}/templates/metadata.yaml.tmpl", {
-  #   hostname = format("%s-tfc-agent", each.value)
-  # })
 }
